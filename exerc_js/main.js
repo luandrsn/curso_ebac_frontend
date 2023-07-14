0fc2105a48eeecf56719d2735e20fcbc24e07248
Luan Silva <santosluan994@hotmail.com>
2023-07-14 00:30:29 -0300

    alteração ocultar mensagem de erro

--- exerc_js/main.js
+++ exerc_js/main.js
@@ -31,10 +31,10 @@ campB.addEventListener('keyup', function(e) {
     formEvalido = validaCampo(e.target.value);
 
     if (!formEvalido) {
-        campA.style.border = '1px solid rgb(103, 94, 198)';
+        campA.classList.add('error');
         document.querySelector('.error-message').style.display = 'block';
     } else {
-        campA.style.border = '';
+        campA.classList.remove('error');
         document.querySelector('.error-message').style.display = 'none';
     }
 });
