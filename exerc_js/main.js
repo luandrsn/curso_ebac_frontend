56942ef6f252b2c00260017c78767a5bfbd2654c
Luan Silva <santosluan994@hotmail.com>
2023-07-13 22:23:02 -0300

    criação de função

--- exerc_js/main.js
+++ exerc_js/main.js
@@ -1,5 +1,8 @@
 const form = document.getElementById('form-comparativo');
 
+function validaCampo(campoA,campoB)
+return campoA<campoB;
+
 form.addEventListener('submit', function(e) {
     e.preventDefault ();
 })
