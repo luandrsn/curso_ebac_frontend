6d2cb23fa16061186a4d223bd1fd846b921af406
Luan Silva <santosluan994@hotmail.com>
2023-07-13 22:59:27 -0300

    ajuste do teste de validação e criação da mensagem de sucesso

--- exerc_js/main.js
+++ exerc_js/main.js
@@ -6,11 +6,14 @@ function validaCampo(campoA,campoB) {
 }
 
 form.addEventListener('submit', function(e) {
+    let formEvalido = false;
     e.preventDefault ();
 
     const campA = document.getElementById('campo-a');
     const campB = document.getElementById('campo-b');
-    if (!validaCampo(campA.value,campB.value)) {
+    const mensagemSucesso =`O valor: ${campB.value} referente a B é maior que o valor: ${campA.value} referente a A`;
+    formEvalido = validaCampo(campA.value,campB.value)
+    if (formEvalido) {
         alert("erro")
     } else {
         alert('tudo ok');
