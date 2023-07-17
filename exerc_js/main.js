c2e56fa8adb1ed5c328b6bf5fa52349f5be8dc35
Luan Silva <santosluan994@hotmail.com>
2023-07-17 18:52:57 -0300

    ajuste para tentar validar o comparativo

--- exerc_js/main.js
+++ exerc_js/main.js
@@ -1,42 +1,43 @@
 const form = document.getElementById('form-comparativo');
 
+let formEvalido = false;
+
 function validaCampo(campoA,campoB) {
-    let campo = campoA < campoB;
-    return campo;
+    return campoB > campoA;
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault ();
 
-    const campA = document.getElementById('campo-a');
-    const campB = document.getElementById('campo-b');
+    let campoA = parseFloat(document.getElementById('campo-a').value);
+    let campoB = parseFloat(document.getElementById('campo-b').value);
 
-    let formEvalido = false;
+    const mensagemSucesso =`O valor <b>${campoB}</b> é maior que o valor <b>${campoB}</b>;`;
 
-    const mensagemSucesso =`O valor <b>${campB.value}</b> é maior que o valor <b>${campA.value}</b>;`;
-    formEvalido = validaCampo(parseFloat(campA.value,campB.value))
+    formEvalido = validaCampo(campoA,campoB)
+
     if (formEvalido) {
         const containerMensagemSucesso = document.querySelector('.success-message');
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display ='block';
 
-        campA.value = '';
-        campB.value = '';
+        campoA = '';
+        campoB = '';
     } else {
-        campB.style.border = '1px solid rgb(103, 94, 198)';
+        campoB.style.border = '1px solid rgb(103, 94, 198)';
         document.querySelector('.error-message').style.display = 'block';
     }
 })
 
-campB.addEventListener('keyup', function(e) {
+campoB.addEventListener('keyup', function(e) {
     console.log(e.target.value);
     formEvalido = validaCampo(e.target.value);
 
     if (formEvalido) {
-        campB.style.border = '1px solid rgb(103, 94, 198)';
+        campoB.style.border = '1px solid rgb(103, 94, 198)';
         document.querySelector('.error-message').style.display = 'block';
     } else {
-        campB.style.border = '';
+        campoB.style.border = '';
         document.querySelector('.error-message').style.display = 'none';
     }
 })
