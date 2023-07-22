3dd341c1418db19e659d7423665820de547bd409
Luan Silva <santosluan994@hotmail.com>
2023-07-22 16:41:09 -0300

    criar função de aparecer form

--- exer_jqry/main.js
+++ exer_jqry/main.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $('form').on('submit', function(e) {
         e.preventDefault();
     })
+
+    $('header buttom').click(function() {
+        $('form').slideDown();
+    })
 })
