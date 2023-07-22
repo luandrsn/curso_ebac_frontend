5d01da0eca6b7124a04525d5ce957fb749196857
Luan Silva <santosluan994@hotmail.com>
2023-07-22 16:52:47 -0300

    função de ocultar form

--- exer_jqry/main.js
+++ exer_jqry/main.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
     $('header button').click(function() {
         $('form').slideDown();
     })
+
+    $('#ocultar-form').click(function() {
+        $('form').slideUp();
+    })
     
     $('form').on('submit', function(e) {
         e.preventDefault();
